7c4cd119356f22e7a07dc7477f0e0e31746a2fd3
zhangx258 <mqbut0879@163.com>
2020-01-07 17:38:23 +0800

    add wings page,navbar and search bar.adjust some button

--- oneblog/blueprints/blog.py
+++ oneblog/blueprints/blog.py
@@ -9,25 +9,26 @@ from flask import Blueprint, render_template, redirect, url_for, abort, request,
 from oneblog.models import *
 from oneblog.forms import *
 from flask_login import login_required
+from sqlalchemy import or_, extract, text
 blog_bp = Blueprint('blog', __name__)
 
 
 @blog_bp.route('/')
 def index():
-    delete = DeletearticleForm()
-    edit = EditarticleForm()
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLUELOG_POST_PER_PAGE']
     pagination = Article.query.order_by(Article.timestamp.desc()).paginate(page, per_page=per_page)
     articles = pagination.items
-    return render_template('blog/index.html', articles=articles, delete=delete, edit=edit, pagination=pagination)
+    return render_template('blog/index.html', articles=articles, pagination=pagination)
 
 
 @blog_bp.route('/article/<int:article_id>')
 @login_required
 def show_article(article_id):
+    delete = DeletearticleForm()
+    edit = EditarticleForm()
     article = Article.query.filter(Article.id == article_id).first()
-    return render_template('blog/article.html', article=article)
+    return render_template('blog/article.html', article=article, delete=delete, edit=edit)
 
 
 @blog_bp.route('/post/', methods=['GET', 'POST'])
@@ -71,21 +72,39 @@ def edit_article(article_id):
         default.category_id = form.category_id.data
         default.timestamp = datetime.now()
         db.session.commit()
-        return redirect(url_for('.index'))
+        return redirect(url_for('blog.show_article', article_id=article_id))
     return render_template('blog/post.html', forms=form)
 
 
 @blog_bp.route('/category/<int:category_id>')
 def show_category(category_id):
     category = Category.query.get_or_404(category_id)
-    delete = DeletearticleForm()
-    edit = EditarticleForm()
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLUELOG_POST_PER_PAGE']
     pagination = Article.query.with_parent(category).order_by(Article.timestamp.desc()).paginate(page,
                                                                                                  per_page=per_page)
     articles = pagination.items
-    return render_template('blog/index.html', articles=articles, delete=delete, edit=edit, pagination=pagination)
+    return render_template('blog/index.html', articles=articles, pagination=pagination)
+
+
+@blog_bp.route('/about_wings')
+def about_wings():
+    return render_template('blog/wings.html')
+
+
+@blog_bp.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        kw = request.form.get('kw')
+        articles = Article.query.filter(
+            or_(Article.title.like("%"+kw+"%") if kw is not None else "",
+                Article.body.like("%" + kw + "%") if kw is not None else "",
+                extract('year', Article.timestamp) == int(kw) if kw is not None and kw.isdigit() else text(""),
+                extract('month', Article.timestamp) == int(kw) if kw is not None and kw.isdigit() else text(""),
+                extract('day', Article.timestamp) == int(kw) if kw is not None and kw.isdigit() else text(""),)
+        ).order_by(Article.timestamp.desc())
+        return render_template('blog/index.html', articles=articles, pagination=None)
+    return redirect(url_for('blog.index'))
 
 
 @blog_bp.route('/404')
